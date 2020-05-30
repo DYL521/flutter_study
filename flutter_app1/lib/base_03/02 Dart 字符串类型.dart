acf8fd3a46f1a7d5201b055bb4a8ca582e4f891e
DYL521 <1016068291@qq.com>
2020-05-30 09:41:09 +0800

    add 03基本数据类型

---
+++ flutter_app1/lib/base_03/02 Dart 字符串类型.dart
@@ -0,0 +1,57 @@
+/*
+Dart数据类型：字符串类型
+*/
+
+
+
+void main(){
+
+  //1、字符串定义的几种方式
+
+    // 1、var 定义数据类型
+    // var str1='this is str1';
+    // var str2="this is str2";
+    // print(str1);
+    // print(str2);
+
+
+
+    // 2、String 定义字符串，最好自定数据类型
+    // String str1='this is str1';
+
+    // String str2="this is str2";
+    // print(str1);
+    // print(str2);
+
+
+    // 3、''' 三个单引号活着三个双引号，可以跨行定义变量，定义字符串
+//     String str1='''this is str1
+//     this is str1
+//     this is str1
+//     ''';
+//      print(str1);
+
+  
+      //   String str1="""
+      //   this is str1
+      //   this is str1
+      //   this is str1
+      //   """;
+      //  print(str1);
+
+
+
+  //2、字符串的拼接
+
+  String str1='你好';
+  String str2='Dart';
+  // print("$str1 $str2");
+
+  print(str1 + str2);
+  print(str1 +" "+ str2);
+
+
+
+
+}
+
