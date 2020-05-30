3fadf60e604d503c3b02a374b1715469b8a8f7a4
DYL521 <1016068291@qq.com>
2020-05-30 14:44:22 +0800

    默认构造函数，命名构造函数，get and set

---
+++ flutter_base/lib/base_09/05 Dart中把类单独抽离成一个模块.dart
@@ -0,0 +1,9 @@
+import 'package:flutterbase/base_09/02%20Dart%E4%B8%AD%E5%88%9B%E5%BB%BA%E4%B9%89%E7%B1%BB%E4%BD%BF%E7%94%A8%E7%B1%BB.dart';
+
+
+void main(){
+
+  Person p1=new Person.setInfo('李四1',30);
+  p1.printInfo(); 
+
+}
