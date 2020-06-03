114c8143a383c6b6d3671bdfe410a2176bb64aa7
DYL521 <1016068291@qq.com>
2020-06-03 22:50:51 +0800

    add container and Text widget ...

--- flutter_app_widget/lib/main.dart
+++ flutter_app_widget/lib/main.dart
@@ -27,11 +27,43 @@ class HomeContent extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Center(
-      child: new Text(
-        "你好,fltter2",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 20.0, color: Colors.blue),
-      ),
+      child: new Container(
+        child: Text("Dart Flutter教程_Dart -2020年新 Dart Flutter教程_Dart Flutter入门实战视频教程-2020年新出-第14讲以后是Flutter教程",
+          textAlign: TextAlign.left,
+          overflow: TextOverflow.ellipsis,
+          maxLines: 2, //最大行数
+          textScaleFactor: 2,//字体放大
+          style: TextStyle(
+            fontSize: 16.0,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,// 字体加粗，
+            fontStyle: FontStyle.italic, //文字
+            decoration: TextDecoration.lineThrough,
+            decorationColor: Colors.white,
+            decorationStyle: TextDecorationStyle.dashed,
+            letterSpacing: 2.0, //字间距
+          ),
+        ),
+        height: 300.0,// 都是double
+        width: 300.0,
+        decoration: BoxDecoration( // 背景
+        color: Colors.yellow,
+        border: Border.all( // 边框
+          color: Colors.blueGrey,
+          width: 2.0
+        ),
+          borderRadius:BorderRadius.all( //设置圆角
+            Radius.circular(8),
+          ),
+        ),
+//        padding: EdgeInsets.all(20),// 边距
+        padding: EdgeInsets.fromLTRB(1, 1, 3, 4),// 边距
+//        transform: Matrix4.translationValues(100, 0, 0),// 位移
+//        transform: Matrix4.rotationZ(0.3),// 旋转，默认顺时针
+        transform: Matrix4.diagonal3Values(1.2,1,1),// 拉伸
+
+        alignment: Alignment.bottomCenter, //对齐 方式
+      )
     );
     ;
   }
